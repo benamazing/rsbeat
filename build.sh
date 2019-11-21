890bb507c4484ae1a13c548705d5a4b2ccb138e1
lixiaobin <lixiaobin@wps.cn>
2019-11-21 16:16:57 +0800

    change beat version to 5.3.2

--- build.sh
+++ build.sh
@@ -2,14 +2,14 @@ export GOPATH=${PWD}/gopath
 rm -rf ${GOPATH}
 rm -rf vendor
 mkdir -p vendor
-curl -fsSL "https://github.com/elastic/beats/archive/v5.3.0.tar.gz" -o vendor/beats-v5.3.0.tar.gz
+curl -fsSL "https://github.com/elastic/beats/archive/v5.3.2.tar.gz" -o vendor/beats-v5.3.2.tar.gz
 curl -fsSL "https://github.com/garyburd/redigo/archive/v1.6.0.tar.gz" -o vendor/redigo-v1.6.0.tar.gz
 
 mkdir -p ${GOPATH}/src/github.com/elastic
 mkdir -p ${GOPATH}/src/github.com/garyburd
 
-tar zxf vendor/beats-v5.3.0.tar.gz -C ${GOPATH}/src/github.com/elastic
-mv ${GOPATH}/src/github.com/elastic/beats-5.3.0 ${GOPATH}/src/github.com/elastic/beats
+tar zxf vendor/beats-v5.3.2.tar.gz -C ${GOPATH}/src/github.com/elastic
+mv ${GOPATH}/src/github.com/elastic/beats-5.3.2 ${GOPATH}/src/github.com/elastic/beats
 
 tar zxf vendor/redigo-v1.6.0.tar.gz -C ${GOPATH}/src/github.com/garyburd
 mv ${GOPATH}/src/github.com/garyburd/redigo-1.6.0 ${GOPATH}/src/github.com/garyburd/redigo
